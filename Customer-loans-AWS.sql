d00a40c3dc3663fe49c76629179258610b68a068
Tori Gilbert <torigilbert@hotmail.com>
2024-12-05 15:36:58 +0000

    new file:   .vscode/settings.json 	new file:   Customer-loans-AWS.sql 	modified:   credentials.yaml 	modified:   db_utils.py

---
+++ Customer-loans-AWS.sql
@@ -0,0 +1,1 @@
+SELECT * FROM pga_job
--- db_utils.py
+++ db_utils.py
@@ -1,1 +1,17 @@
-class RDSDatabaseConnector (self):
+import psycopg2
+import pandas as pd
+import numpy as np
+import yaml
+
+with open('credentials.yaml','r') as file:
+    credentials=yaml.safe_load(file)
+
+conn = psycopg2.connect(
+    host=credentials['RDS_HOST'],
+    port=credentials['RDS_PORT'],
+    database=credentials['RDS_DATABASE'],
+    user=credentials['RDS_USER'],
+    password=credentials['RDS_PASSWORD'])
+
+cursor = conn.cursor()
+conn.close()
